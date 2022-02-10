20cf5667909c63683264114f1bb43dc6e3a704c6
nabdallahsaab <97283136+nabdallahsaab@users.noreply.github.com>
2022-02-10 12:29:17 +0100

    Update st_iris.py

--- st_iris.py
+++ st_iris.py
@@ -10,11 +10,7 @@ import streamlit as st
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier,plot_tree
-from sklearn.datasets import make_moons
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
+
 arr = np.random.normal(1, 1, size=100)
 st.write("Hello !")
 fig, ax = plt.subplots()
