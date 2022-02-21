6219e70a1843456e1884aa0e4db6a685536a72b5
nabdallahsaab <97283136+nabdallahsaab@users.noreply.github.com>
2022-02-21 11:25:00 +0100

    Add files via upload

---
+++ streamlit_app_cam.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 11:19:51 2022
+
+@author: isen
+"""
+import cv2
+import streamlit as st
+
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
