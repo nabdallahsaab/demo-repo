0fdd8e7920ef6870324a164d4ade663795429409
nabdallahsaab <97283136+nabdallahsaab@users.noreply.github.com>
2022-02-21 13:34:40 +0100

    Add files via upload

---
+++ streamlit_app_webrtc.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 13:21:11 2022
+
+@author: isen
+"""
+
+
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+
+webrtc_streamer(key="example")
